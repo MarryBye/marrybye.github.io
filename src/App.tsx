c9dadf32c01481daf7eb4d2ff2117609642f767c
MarryBye <vlukianov04@gmail.com>
2026-06-25 18:06:28 +0300

    Trying to fix github-pages...

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Routes, Route, BrowserRouter } from 'react-router';
+import { Routes, Route, BrowserRouter, HashRouter } from 'react-router';
 import { HomePage, AboutPage } from '@/pages';
 
 const AppPortfolio = () => {
     return (
         <React.StrictMode>
-            <BrowserRouter>
+            <HashRouter>
                 <Routes>
                     <Route path='/' element={<HomePage />} />
                     <Route path='/about' element={<AboutPage />} />
                 </Routes>
-            </BrowserRouter>
+            </HashRouter>
         </React.StrictMode>
     );
 }
